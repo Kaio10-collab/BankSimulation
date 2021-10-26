d2c53989a0f4b6036efbaeca0d1381d5ff624575
kaio10-collab <sancheskaio@gmail.com>
2021-10-26 10:44:39 -0300

    creating packages and files

---
+++ src/main/java/com/SimulacaoBancaria/simulacaoBancaria/SimulacaoBancariaApplication.java
@@ -0,0 +1,13 @@
+package com.SimulacaoBancaria.simulacaoBancaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulacaoBancariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulacaoBancariaApplication.class, args);
+	}
+
+}
