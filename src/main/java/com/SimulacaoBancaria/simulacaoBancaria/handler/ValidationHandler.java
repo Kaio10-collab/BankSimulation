9467663c38924c5d491eee61f34f740a54abbabb
kaio10-collab <sancheskaio@gmail.com>
2022-01-04 14:35:01 -0300

    subindo teste de branch

---
+++ src/main/java/com/SimulacaoBancaria/simulacaoBancaria/handler/ValidationHandler.java
@@ -0,0 +1,9 @@
+package com.SimulacaoBancaria.simulacaoBancaria.handler;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+public class ValidationHandler extends ResponseEntityExceptionHandler {
+
+}
